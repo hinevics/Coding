e614a81589c537e8262ec3b6499066eabbb04ecc
hinevics <hinevics@gmail.com>
2021-03-16 00:09:04 +0300

    add functions

--- inverted_index/inverted_index.py
+++ inverted_index/inverted_index.py
@@ -1,28 +1,45 @@
 import pandas as pd
 import json
+from collections import defaultdict
 
 work_links = {
-    'dataset': r'.\wikipedia_sample',
-    'stop_words': r'.\stop_words_en.txt',
+    'dataset': r'D:\Development\Coding\Coding\Data\wikipedia_sample',
+    'stop_words': r'D:\Development\Coding\Coding\Data\stop_words_en.txt',
     'invert_index': ...}
 
 
-class InvertedIndex(object):
+class InvertedIndex:
+    def __init__(self, word_to_docs_mapping):
+        self.word_to_docs_mapping = {
+            word: doc_ids for word, doc_ids in word_to_docs_mapping.items()}
+
     def query(self, words):
-        pass
+        raise NotImplementedError
 
     def dump(self, filepath):
-        pass
+        # загрузка на диск
+        with open(file=filepath, mode='w') as file:
+            words_doc_ids = {
+                word: list(ids) for word, ids in self.word_to_docs_mapping.items()}
+            json.dump(words_doc_ids, file)
 
     @classmethod
     def load(cls, filepath):
-        pass
+        # считывем с диска
+        with open(file=filepath, mode='r') as file:
+            return {index: set(words) for index, words in json.load(fp=file).items()}
 
 
 def load_documents(filepath):
     with open(file=filepath, encoding='utf-8') as file:
-        result = file.readlines()
-    return result
+        documents = file.readlines()
+    index, words = [], []
+    for document in map(lambda x: x.split(' '), documents):
+        # через map разбиваю документ (ввиде целой стороки разеделнной пробелами) на список символов
+        index_doc = int(document.pop(0).split('\t')[0])  # 0 это индекс назавания статьи и index'a документа
+        index.append(index_doc)
+        words.append(set([i.lower() for i in document if i.isalpha()]))
+    return index, words
 
 
 def load_stop_words(filepath):
@@ -31,23 +48,26 @@ def load_stop_words(filepath):
     return result
 
 
-def build_inverted_index(documents, stop_words):
-    # id_words words id_doc
-    # id_doc
-    # allwords_alldocs_list -- unique --  sorted -- del stop_words -- index
-    for document in map(lambda x: x.split(' '), documents):
-        # через map разбиваю документ (ввиде целой стороки разеделнной пробелами) на список символов
-        index = document.pop(0).split('\t')  # 0 это индекс назавания статьи и index'a документа
-        print(index)
-        break
+def build_inverted_index(indexs, words, stop_words):
+    inverted_index = dict()
+    for word, index in zip(words, indexs):
+        word.difference_update(stop_words)
+        for w in word:
+            if not (w in inverted_index.keys()):
+                inverted_index[w] = {index}
+            else:
+                inverted_index[w].update({index})
+    return InvertedIndex(inverted_index)
 
 
 def main():
-    documents = load_documents(work_links['dataset'])
+    indexs, words = load_documents(work_links['dataset'])
     stop_words = load_stop_words(work_links['stop_words'])
-    # inverted_index = build_inverted_index(documents, stop_words)
-    # inverted_index.dump("inverted.index")  # json
-    # inverted_index = InvertedIndex.load("inverted.index")
+    inverted_index = build_inverted_index(indexs=indexs, words=words, stop_words=stop_words)
+    inverted_index.dump("inverted.index")  # json записывается на диск
+    inverted_index = InvertedIndex.load("inverted.index")
+    print(inverted_index['diggers'])
+
     # document_ids = inverted_index.query(["two", "words"])
 
 
