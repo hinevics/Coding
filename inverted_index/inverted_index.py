a78fe034c78a0c16452be870c6800a812868f2cf
hinevics <hinevics@gmail.com>
2021-03-23 23:08:30 +0300

    add new test and add dump for class

--- inverted_index/inverted_index.py
+++ inverted_index/inverted_index.py
@@ -1,4 +1,7 @@
 import json
+from re import sub
+import os.path
+
 # from collections import defaultdict
 
 work_links = {
@@ -16,11 +19,30 @@ class InvertedIndex:
         raise NotImplementedError
 
     def dump(self, filepath):
-        # загрузка на диск
+        # преобразую set в list для записи в json
+        words_doc_ids = {
+            word: list(ids) for word, ids in self.word_to_docs_mapping.items()}
+        # проверка файла
+        check_file = os.path.isfile(filepath)
+        if check_file:
+            with open(file=filepath, mode='r') as file:
+                words_with_file = json.load(fp=file)
+        else:
+            words_with_file = dict()  # пустой словарь для файла которого нет
+
+        # update dict
+        if set(words_doc_ids.keys()).isdisjoint(set(words_with_file.keys())):
+            words_with_file.update(words_doc_ids)
+        else:
+            keys_intersection = set(words_doc_ids.keys()) & set(words_with_file.keys())
+            keys_difference = set()
+            for key in keys_intersection:
+                words_with_file[key] += words_doc_ids[key]
+            for key in keys_difference:
+                words_with_file[key] = words_doc_ids[key]
+        # запись в файл
         with open(file=filepath, mode='w') as file:
-            words_doc_ids = {
-                word: list(ids) for word, ids in self.word_to_docs_mapping.items()}
-            json.dump(words_doc_ids, file)
+            json.dump(words_with_file, file)
 
     @classmethod
     def load(cls, filepath):
@@ -31,19 +53,20 @@ class InvertedIndex:
 
 def load_documents(filepath):
     with open(file=filepath, encoding='utf-8') as file:
-        documents = file.readlines()
+        documents = set(sub(r'[\.\,\!\?]', '', file.read()).split('\n'))
+        documents.discard('')
     index, words = [], []
-    for document in map(lambda x: x.split(' '), documents):
+    for document in map(lambda x: x.split('\t'), documents):
         # через map разбиваю документ (ввиде целой стороки разеделнной пробелами) на список символов
-        index_doc = int(document.pop(0).split('\t')[0])  # 0 это индекс назавания статьи и index'a документа
-        index.append(index_doc)
-        words.append(set([i.lower() for i in document if i.isalpha()]))
+        # 0 это индекс назавания статьи и index'a документа
+        index.append(int(document[0]) if document[0] != '' else [])
+        words.append(set([i.lower() for i in document[1].split(' ') if i.isalpha()]) if document[0] != '' else {})
     return index, words
 
 
 def load_stop_words(filepath):
     with open(file=filepath, encoding='utf-8') as file:
-        result = set(map(lambda x: x.strip('\n'), file.readlines()))
+        result = set(map(lambda x: x.lower().strip('\n'), file.readlines()))
     return result
 
 
@@ -65,7 +88,7 @@ def main():
     inverted_index = build_inverted_index(indexs=indexs, words=words, stop_words=stop_words)
     inverted_index.dump("inverted.index")  # json записывается на диск
     inverted_index = InvertedIndex.load("inverted.index")
-    print(inverted_index['diggers'])
+    # print(inverted_index['diggers'])
 
     # document_ids = inverted_index.query(["two", "words"])
 
--- inverted_index/test_inverted_index.py
+++ inverted_index/test_inverted_index.py
@@ -2,20 +2,15 @@ import pytest
 import inverted_index
 
 
-def test_can_import_inverted_index_module():
+def test_can_import_inverted_index_module1():
     import inverted_index
 
 
-def test_can_load_stop_words():
+def test_can_load_load_documents2():
     assert inverted_index.load_documents
 
 
-def test_can_read_documents():
-    test_link = inverted_index.work_links['dataset']
-    assert inverted_index.load_documents(filepath=test_link)
-
-
-def test_len_document():
+def test_len_document3():
     """Unit Testing"""
     # Проверка длины документы
     test_link = inverted_index.work_links['dataset']
@@ -24,29 +19,145 @@ def test_len_document():
         assert len(data) == 4100
 
 
-def test_import_fuction_load_documents():
-    test_link = inverted_index.work_links['dataset']
-    assert inverted_index.load_documents(filepath=test_link)
-
-
-def test_work_function_load_documents_with_one_doc(tmpdir):
+def test_work_function_load_documents_with_one_doc4(tmpdir):
     test_doc = tmpdir.join('datatest.txt')  # создаю временный тестовый документ
-    test_doc.write('0\tTest text testing my work now!') # временные тестовые данные
+    # временные тестовые данные
+    test_doc.write('0\tTest text testing, my work now! Train testing this, working, bool?')
     test_index, test_words = inverted_index.load_documents(test_doc)
     etalon_index = [0]
-    etalon_words = [{'test', 'text', 'testing', 'my', 'work'}]
+    etalon_words = [{'test', 'text', 'testing', 'my', 'work', 'now', 'train', 'this', 'working', 'bool'}]
     assert etalon_index == test_index
-    assert etalon_words[0] == etalon_words[0]
+    assert etalon_words == test_words
 
 
-def test_work_function_load_documents_with_lot_doc(tmpdir):
+def test_work_function_load_documents_with_lot_doc5(tmpdir):
     test_doc = tmpdir.join('datatest.txt')  # создаю временный тестовый документ
-    test_doc.write('0\tTest text test number one\n'
-                   '1\tTest text number two') # временные тестовые данные
+    test_doc.write('0\tTest text! test number one...\n1\tTest text... number two!')
     test_index, test_words = inverted_index.load_documents(test_doc)
-    etalon_index = [0, 1]
-    etalon_words = [{'test', 'text', 'number', 'one'},
-                    {'test', 'text', 'number', 'two'}]
-    assert etalon_index == test_index
-    for etalon, test in zip(etalon_words, test_words):
-        assert etalon == test
+    etalon_index = {0, 1}
+    etalon_words = {0: {'test', 'text', 'number', 'one'},
+                    1: {'test', 'number', 'two', 'text'}}
+    assert etalon_index == set(test_index)
+    for text, index in zip(test_words, test_index):
+        assert text == etalon_words[index]
+
+
+def test_work_function_load_documents_with_an_extra_newline_character6(tmpdir):
+    test_doc = tmpdir.join('datatest.txt')  # создаю временный тестовый документ
+    test_doc.write('0\tTest text! test number one...\n1\tTest text... number two!\n')
+    test_index, test_words = inverted_index.load_documents(test_doc)
+    etalon_index = {0, 1}
+    etalon_words = {0: {'test', 'text', 'number', 'one'},
+                    1: {'test', 'number', 'two', 'text'}}
+    assert etalon_index == set(test_index)
+    for text, index in zip(test_words, test_index):
+        assert text == etalon_words[index]
+
+
+def test_work_function_load_documents_with_lot_doc7(tmpdir):
+    test_doc = tmpdir.join('datatest.txt')  # создаю временный тестовый документ
+    test_doc.write('0\tTest text! test number one...\n1\tTest text... number two!\n'
+                   '2\tKent!!!! red gay\n3\tBoys len lan\n12\tTrest!!! best wreit!')
+    test_index, test_words = inverted_index.load_documents(test_doc)
+    etalon_index = {0, 1, 2, 3, 12}
+    etalon_words = {0: {'test', 'text', 'number', 'one'},
+                    1: {'test', 'number', 'two', 'text'},
+                    2: {'kent', 'red', 'gay'},
+                    3: {'boys', 'len', 'lan'},
+                    12: {'trest', 'best', 'wreit'}}
+    assert etalon_index == set(test_index)
+    for text, index in zip(test_words, test_index):
+        assert text == etalon_words[index]
+
+
+def test_load_stop_words8(tmpdir):
+    test_doc = tmpdir.join('datatest.txt')
+    test_doc.write('Test\ntext\ntest\nnumber\none\nTe\ntext\nnumber2\ntwo')
+    test_words = inverted_index.load_stop_words(test_doc)
+    etalon_words = {'test', 'text', 'number', 'one', 'te', 'number2', 'two'}
+    assert etalon_words == test_words
+
+
+def test_load_stop_words_with_an_extra_newline_character9(tmpdir):
+    test_doc = tmpdir.join('datatest.txt')  # создаю временный тестовый документ
+    test_doc.write('Test\ntext\ntest\nnumber\none\nTe\ntext\nnumber2\ntwo\n')
+    test_words = inverted_index.load_stop_words(test_doc)
+    etalon_words = {'test', 'text', 'number', 'one', 'te', 'number2', 'two'}
+    assert etalon_words == test_words
+
+
+def test_build_inverted_index10():
+    assert inverted_index.build_inverted_index
+
+
+def test_build_inverted_index11():
+    test_index = [0, 1, 3]
+    test_words = [{'test', 'null', 'hi'}, {'test', 'one', 'lol'}, {'test', 'two', 'cant'}]
+    test_stop_words = {'hi', 'cant', 'lol'}
+    test_result = inverted_index.build_inverted_index(indexs=test_index, stop_words=test_stop_words,
+                                                      words=test_words)
+    assert test_result.word_to_docs_mapping == {'test': {0, 1, 3},
+                                                'null': {0},
+                                                'one': {1},
+                                                'two': {3}}
+
+
+def test_build_inverted_index12():
+    test_index = [0, 1, 3]
+    test_words = [{'test', 'null', 'hi'}, {'test', 'one', 'lol'}, {'test', 'two', 'cant'}]
+    test_stop_words = {'hi', 'cant', 'lol'}
+
+    test_index2 = [15, 25]
+    test_words2 = [{'one'}, {'two'}]
+    test_result = inverted_index.build_inverted_index(indexs=test_index, stop_words=test_stop_words,
+                                                      words=test_words)
+    inverted_index.build_inverted_index(indexs=test_index2,
+                                        stop_words=test_stop_words,
+                                        words=test_words2)
+
+    assert test_result.word_to_docs_mapping == {'test': {0, 1, 3},
+                                                'null': {0},
+                                                'one': {1},
+                                                'two': {3}}
+
+
+def test_build_inverted_index13(tmp_path):
+    f1 = tmp_path / 'temp'
+    test_index = [0, 1, 3]
+    test_words = [{'test', 'null', 'hi'}, {'test', 'one', 'lol'}, {'test', 'two', 'cant'}]
+    test_stop_words = {'hi', 'cant', 'lol'}
+    test_result = inverted_index.build_inverted_index(indexs=test_index, stop_words=test_stop_words,
+                                                      words=test_words)
+    test_result.dump(filepath=f1)
+    result = inverted_index.InvertedIndex.load(filepath=f1)
+    assert result == {
+        'test': {0, 1, 3},
+        'null': {0},
+        'one': {1},
+        'two': {3}
+    }
+
+
+def test_build_inverted_index14(tmp_path):
+    f1 = tmp_path / 'temp'
+    test_index = [0, 1, 3]
+    test_words = [{'test', 'null', 'hi'}, {'test', 'one', 'lol'}, {'test', 'two', 'cant'}]
+    test_stop_words = {'hi', 'cant', 'lol'}
+    test_result = inverted_index.build_inverted_index(indexs=test_index, stop_words=test_stop_words,
+                                                      words=test_words)
+    test_result.dump(filepath=f1)
+
+    test_index2 = [15, 25]
+    test_words2 = [{'one'}, {'two'}]
+    test_result2 = inverted_index.build_inverted_index(indexs=test_index2, stop_words=test_stop_words,
+                                                       words=test_words2)
+
+    test_result2.dump(filepath=f1)
+
+    result = inverted_index.InvertedIndex.load(filepath=f1)
+    assert result == {
+        'test': {0, 1, 3},
+        'null': {0},
+        'one': {1, 15},
+        'two': {3, 25}
+    }
